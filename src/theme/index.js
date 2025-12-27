9ddb2218f698f63dcd081526e667c7cf530a1e04
Maryam <161246099+maryamarshadqaimkhani@users.noreply.github.com>
2025-12-28 01:55:10 +0500

    Update and rename Root.js to index.js

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@theme-original/Layout';
+import Chatbot from '../../components/Chatbot';
+
+export default function LayoutWrapper(props) {
+  return (
+    <>
+      <Layout {...props} />
+      <Chatbot />
+    </>
+  );
+}
